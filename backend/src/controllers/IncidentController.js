1cc64ccc2b419b2f89d674221f60153ca437957b
danielmorita <danielkeyti@gmail.com>
2020-03-24 23:35:20 -0300

    cors, aula2

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -19,9 +19,17 @@ module.exports = {
         const { page = 1 } = req.query;
         const [count] = await connection('incidents').count();
         const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
-        .select('*');
+        .select([
+            'incidents.*', 
+            'ongs.name', 
+            'ongs.email', 
+            'ongs.whatsapp', 
+            'ongs.city', 
+            'ongs.uf'
+        ]);
 
         res.header('X-Total-Count', count['count(*)']);
         res.json(incidents);
